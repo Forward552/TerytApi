167c814bee4cd57a78df87af299e379f692032e2
Francuz94 <Forward552@gmail.com>
2024-03-19 16:29:26 +0100

    Dodaj pliki projektów.

---
+++ TerytApi/Client/TerytConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace TerytWsApi.Client
+{
+    public static class TerytConfig
+    {
+        public static void SetTerytConfig(string login, string haslo, string url)
+        {
+            LoginTeryt = login;
+            HasloTeryt = haslo;
+            UslugaTeryt = url;
+        }
+        public static string LoginTeryt { get; private set; } = "TestPubliczny";
+        public static string HasloTeryt { get; private set; } = "1234abcd\r\n";
+        public static string UslugaTeryt { get; private set; } = "https://uslugaterytws1test.stat.gov.pl/terytws1.svc";
+
+    }
+}
---
+++ TerytApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TerytApi;
+using TerytWsApi.Client;
+
+
+
+TerytWs1Client client = new TerytWs1Client(TerytWs1Client.EndpointConfiguration.custom, TerytConfig.UslugaTeryt);
+client.ClientCredentials.UserName.UserName = TerytConfig.LoginTeryt;
+client.ClientCredentials.UserName.Password = TerytConfig.HasloTeryt;
+var test = await client.PobierzListeWojewodztwAsync(DateTime.Now);
+
+Console.WriteLine("Koniec");
